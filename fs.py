de48eea0680980e0691d7178dbd012f517069b91
Anton Lukyanov <anton.lukyanov@gmail.com>
2019-06-13 13:18:15 +0300

    Updated makedirs function.

--- fs.py
+++ fs.py
@@ -5,5 +5,8 @@ Common file system utilities.
 import os
 
 
-def makedirs(dirpath, mode=0o700):
-    os.makedirs(dirpath, mode, True)
+def makedirs(dirpaths, mode=0o700):
+    if isinstance(dirpaths, str):
+        dirpaths = [dirpaths]
+    for dp in dirpaths:
+        os.makedirs(dp, mode, True)
