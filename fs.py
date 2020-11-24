b43441254599007e486a912cba23276e1654a0d6
Anton Lukyanov <anton.lukyanov@gmail.com>
2020-11-24 20:55:57 +0300

    Added exist_ok argument to makedirs().

--- fs.py
+++ fs.py
@@ -5,10 +5,10 @@ Common file system utilities.
 import os
 
 
-def makedirs(*args, mode=0o755):
+def makedirs(*args, mode=0o755, exist_ok=True):
     for path in args:
         if isinstance(path, list) or isinstance(path, tuple):
             for dp in path:
-                os.makedirs(dp, mode, True)
+                os.makedirs(dp, mode, exist_ok)
         else:
-            os.makedirs(path, mode, True)
+            os.makedirs(path, mode, exist_ok)
